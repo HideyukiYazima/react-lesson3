8fdebfdc3e0c2ce1c83d3faf9e14f81a560f5a4e
Yazima <yazima04200@gmail.com>
2022-10-02 23:43:17 +0900

    送信完了表示の追加

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,24 +1,40 @@
 import React from 'react';
 
 class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmitted: false
+    };
+  }
+
   render() {
-    return (
-      <div className='contact-form'>
+    let contactForm;
+
+    if (this.state.isSubmitted) {
+      contactForm = (
+        <div className='contact-submit-message'>
+          送信完了
+        </div>
+      );
+    } else {
+      contactForm = (
         <form>
           <p>メールアドレス（必須）</p>
-
           <input />
-          
           <p>お問い合わせ内容（必須）</p>
-
           <textarea />
-
           <input
             type='submit'
             value='送信'
-            />
-          
+          />
         </form>
+      );
+    }
+    
+    return (
+      <div className='contact-form'>
+        {contactForm}
       </div>
     );
   }
